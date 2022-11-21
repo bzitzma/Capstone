1439a9832ce4536c06243ca5baec57143cc20331
Brian J Zitzmann <bzitzma@tulane.edu>
2022-11-21 02:19:25 -0600

    Update SouthBalance.TicketAssignment.sql

--- Tables/SouthBalance.TicketAssignment.sql
+++ Tables/SouthBalance.TicketAssignment.sql
@@ -6,5 +6,5 @@ CREATE TABLE [SouthBalance].[TicketAssignment](
 	[IsDeleted] [bit] CONSTRAINT [DF_TicketAssignment_IsDeleted] DEFAULT ((0)) NULL,
 	CONSTRAINT [PK_TicketAssignment] PRIMARY KEY CLUSTERED ([TicketAssignmentKey] ASC),
 	CONSTRAINT [FK_TicketAssignment_Person] FOREIGN KEY ([PersonKey]) REFERENCES [SouthBalance].[Person] ([PersonKey]),
-	CONSTRAINT [FK_TicketAssignment_Ticket] FOREIGN KEY ([TicketKey]) REFERENCES [SouthBalance].[Ticket] ([TicketKey]),
+	CONSTRAINT [FK_TicketAssignment_Ticket] FOREIGN KEY ([TicketKey]) REFERENCES [SouthBalance].[Ticket] ([TicketKey])
 );
